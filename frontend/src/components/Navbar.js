12cb1903acf43a26dbcca1146151ef7f122a82f8
avabakirovn-web <avabakirovn@gmail.com>
2026-06-30 14:37:39 +0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -47,7 +47,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2" data-testid="nav-logo">
-            <Logo size="md" />
+            <Logo size="xl" />
           </Link>
 
           {/* Search Bar - Desktop */}
