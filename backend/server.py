ed2211458b9314632ce4e1066850a5d230230cba
avabakirovn-web <avabakirovn@gmail.com>
2026-06-30 13:20:03 +0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1101,3 +1101,8 @@ async def startup_db():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
